43147f9a54038baf98389a022edfd16c3a1c41ff
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-02-11 11:58:32 +0700

    fix: update code

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log(process.env.TEST_DATABASE_URI);
-    const conn = await mongoose.connect(process.env.TEST_DATABASE_URI);
+    console.log(process.env.DATABASE_URI);
+    const conn = await mongoose.connect(process.env.DATABASE_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
