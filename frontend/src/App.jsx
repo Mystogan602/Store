ef84be9f0dbb6cb502baabf2359a1b59d583f3ea
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-02-13 00:14:42 +0700

    setup: router react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Navigation from './pages/Auth/Navigation'
+import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className='py-3'>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
