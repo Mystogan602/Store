f222db51edf6f044160fd2380863cee2c97e6037
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-02-16 20:54:10 +0700

    feat: navbar ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navigation from './pages/Auth/Navigation'
 import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
